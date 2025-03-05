64376fdedc83c296b55f10f6a12b26d5398fc521
TanguyB <tanguy.bodin@student.isae-supaero.fr>
2025-03-05 18:47:49 +0100

    Modifications + usd added

---
+++ SolarPanel and Dust/.ipynb_checkpoints/Panel_Class-checkpoint.py
@@ -0,0 +1,236 @@
+import omni.isaac.core.utils.mesh as mesh_utils
+import omni.isaac.core.utils.prims as prims_utils
+import omni.isaac.core.utils.rotations as rotations_utils
+from omni.physx import get_physx_scene_query_interface
+import numpy as np
+#from Lunar_Dust import LunarDust
+
+
+class Panel() :
+    """
+    Class responsible for keeping track of the battery status
+    prim_path (string) : Path to the panel prim
+    sun_path (string) : Path to the Sun
+    max_storage (int) : Battery capacity (Wh)
+    current_storage (int) : Actual charge of the battery (Wh) 
+    power_consumption (int) : Power consumed by the rover (W)
+    power_produced (int) : Power produced by the rover (W)
+    dust_coverage (float) : Percentage of the panel surface covered by dust (between 0 and 1)
+    """
+    def __init__(self, panel_prim_path, sun_path = "/World/Sun", nb_cells = 100, max_storage = 1000, 
+                 current_storage = 0, power_consumption = 10, power_produced = 0, dust_coverage = 0,
+                 dust = False, timeline = None) :
+        self.prim_path = panel_prim_path
+        self.sun_path = sun_path
+        self.length, self.width, self.height = prims_utils.get_prim_attribute_value(panel_prim_path, "xformOp:scale")
+        self.panel_surface = self.length * self.width
+        self.nb_cells = nb_cells
+        self.cell_area = self.panel_surface / self.nb_cells
+        self.max_storage = max_storage
+        self.current_storage = current_storage
+        self.power_consumption = power_consumption
+        self.current_charge = round(100*current_storage/max_storage, 2)
+        self.power_produced = power_produced
+
+        self.dust = dust
+        self.dust_coverage = dust_coverage
+        if dust :
+            self.lunar_dust = LunarDust()
+            self.prev_simulation_time = 0
+            self.timeline = timeline
+
+    def update(self, stage, time_elapsed) :
+        """
+        Execute one simulation step for the solar panel (currently doesn't support moving the panel to follow the sun)
+
+        Args :
+            time_elapsed : time elapsed since last update
+            stage : The world stage
+
+        Returns :
+            None
+        """
+
+        if self.dust :
+            self.lunar_dust.update(self.timeline.get_current_time(), self.prim_path)
+            self.dust_coverage = self.lunar_dust.coverage
+        self.update_power_produced(stage)
+        self.update_state(time_elapsed)
+        
+    
+    def update_state(self, time_elapsed) :
+        """
+        Simulate battery evolution during time_elapsed
+
+        Args :
+            time_elapsed : time elapsed since last state update
+
+        Returns :
+            None
+        """
+        self.current_storage += (self.power_produced - self.power_consumption)* (time_elapsed / 3600)
+        if self.current_storage > self.max_storage :
+            self.current_storage = self.max_storage
+        if self.current_storage < 0 :
+            self.current_storage = 0
+        self.current_charge = round(100*self.current_storage/self.max_storage, 2)
+        if self.current_storage == 0 :
+            print("Battery is empty")
+
+    def update_power_produced(self, stage) :
+        """
+        Update the power produced by the solar pannel based on the stage state
+
+        Args : 
+            stage : The world stage
+
+        Returns :
+            None
+        """
+        face_vertex_indices = prims_utils.get_prim_attribute_value(self.prim_path, "faceVertexIndices")
+        # Get mesh vertices
+        mesh_prim = stage.GetPrimAtPath(self.prim_path)
+        world_prim = stage.GetPrimAtPath("/World")
+        points_world = mesh_utils.get_mesh_vertices_relative_to(mesh_prim, world_prim)
+
+        # Get quad centers
+        num_quads = len(face_vertex_indices) // 4
+        quad_centers = np.array([np.mean(points_world[list(face_vertex_indices[i * 4 : (i + 1) * 4])], axis=0) for i in range(num_quads)])
+        # In this case we know that the first 100 elements are the faces on the top of the pannel, be careful with other usd.
+        quad_centers = quad_centers[:self.nb_cells] # Keep only faces on the top -> avoid unnecessary computations
+        dot_products = self.compute_sun_to_mesh(quad_centers)
+        
+        power_generation = self.compute_power(dot_products)
+        self.power_produced = power_generation
+
+    def set_power_produced(self, power_produced) :
+        """
+        Manually change the power produced
+
+        Args :
+            power_consumed (float) : The power produced by the panel
+
+        Returns :
+            None
+        """
+        self.power_produced = power_produced
+
+    def set_power_consumed(self, power_consumed) :
+        """
+        Manually change the power consumed
+
+        Args :
+            power_consumed (float) : The power consumed by the electrics
+
+        Returns :
+            None
+        """
+        self.power_consumed = power_consumed
+
+    def set_panel_orientation_euler(self, angles, degrees = True) :
+        """
+        Set the panel orientation using Euler angles.
+
+        Args:
+            angles (tuple or list): The Euler angles (in radians or degrees) to set the orientation.
+            degrees (bool, optional): If True, the angles are interpreted as degrees.
+                                      If False, the angles are interpreted as radians.
+    
+        Returns:
+            None
+        """
+        prims_utils.set_prim_attribute_value(
+            self.prim_path, "xformOp:orient",
+            rotations_utils.euler_angles_to_quat(angles, degrees=degrees),
+        )
+        
+    def display_state(self) :
+        """
+        Display the current state of the battery
+        """
+        print(f"Battery is at {self.current_charge}%, actual production (W) : {self.power_produced}, actual consumption (W): {self.power_consumption} | current_storage (Wh) : {self.current_storage} ")
+
+    def compute_sun_to_mesh(self, mesh_face_centers, output="dotproducts"):
+        """
+        Uses ray-tracing to compute the dot products between the normals of mesh face centers and the direction from the Sun to each face center.
+    
+        Parameters:
+        mesh_face_centers (array-like): An array of 3D coordinates representing the centers of the mesh faces.
+        path_sun (str): The path to the Sun object in the scene. Default is "/World/Sun".
+        output (str): The type of output to return. Options are "dotproducts", "dotproducts_normals", or "angles_deg".
+                      Default is "dotproducts".
+    
+        Returns:
+        numpy.ndarray or tuple: Depending on the output parameter:
+            - "dotproducts": Returns an array of dot products.
+            - "dotproducts_normals": Returns a tuple containing an array of dot products and an array of normals.
+            - "angles_deg": Returns an array of angles in degrees.
+    
+        Notes:
+        - If a mesh face center is in the shadow of an obstruction, the corresponding dot product and normal are set to NaN.
+        - If no hit is detected for a mesh face center, the corresponding dot product and normal are set to NaN.
+        - The function prints a message if no hit is detected for a mesh face center.
+        """
+        sun_coord = prims_utils.get_prim_attribute_value(self.sun_path, "xformOp:translate")
+    
+        dot_products = np.zeros(len(mesh_face_centers))
+        normals = np.zeros((len(mesh_face_centers), 3))
+        hit_distances = []  # for debug
+    
+        for i, center in enumerate(mesh_face_centers):
+            vector_to_cell = center - sun_coord
+            distance = np.linalg.norm(vector_to_cell)
+            direction = vector_to_cell / distance
+    
+            # Cast a ray from the Sun to the cell center
+            hit_info = get_physx_scene_query_interface().raycast_closest(
+                sun_coord, direction, distance + 1
+            )  # increase max distance to avoid ray not reaching the surface
+    
+            if hit_info["hit"]:
+                hit_distance = np.linalg.norm(hit_info["position"] - center)
+                hit_distances.append(hit_distance)
+                if (
+                    hit_distance < 0.001
+                ):  # consider that there is no obstruction between the sun and the cell center
+                    normals[i] = np.array(hit_info["normal"])
+                    dot_products[i] = np.dot(
+                        normals[i], -1 * direction
+                    )  # direction is sun to center, so need to flip it
+                else:  # cell is in shadow of obstruction
+                    dot_products[i] = np.nan
+                    normals[i] = np.nan
+            else:
+                print(f"no hit, angle for cell center at {center} is undefined")
+                dot_products[i] = np.nan  # No hit, angle is undefined
+                normals[i] = np.nan
+    
+        if output == "dotproducts":
+            return dot_products
+        if output == "dotproducts_normals":
+            return dot_products, normals
+        elif output == "angles_deg":
+            sun_angles = np.arccos(dot_products) * (180 / np.pi)  # Convert to degrees
+            return sun_angles
+        else:
+            print(
+                "possible outputs are only 'dotproducts' , 'dotproducts_normals' or 'angles_deg'"
+            )
+            return
+            
+    def compute_power(self, cos_theta) :
+        """
+        Compute the power produced by the solar panel
+
+        Args : 
+            cos_theta : array of the cosine of the incidence angle for each solar cell
+
+        Returns :
+            Power produced by the solar panel
+        """
+        # P = A x I x n x cos(theta) x covering
+        solar_intensity = 1376 # (W/m2)
+        solar_cell_efficiency = 0.3 
+        cos_theta[cos_theta < 0] = 0
+        power_produced = self.cell_area * solar_intensity * solar_cell_efficiency * cos_theta * (1 - self.dust_coverage)
+        return np.nansum(power_produced[:self.nb_cells])
---
+++ SolarPanel and Dust/Lunar_Dust.py
@@ -0,0 +1,251 @@
+import numpy as np
+from scipy.interpolate import PchipInterpolator
+import typing
+import matplotlib.pyplot as plt
+import carb
+from pprint import pprint
+
+from pxr import Usd, UsdGeom, Gf
+from omni.isaac.core import World
+from omni.isaac.core.objects import DynamicCuboid, DynamicSphere, DynamicCone
+from omni.timeline import get_timeline_interface
+from omni.isaac.core.utils.stage import get_current_stage
+import omni.isaac.core.utils.prims as prims_utils
+import omni.isaac.core.utils.mesh as mesh_utils
+from omni.usd import get_prim_at_path
+from omni.physx import get_physx_scene_query_interface  # for raycasting
+
+
+class LunarDust():
+
+    def __init__(self, world):
+        """
+        Initializes the lunar dust accumulation simulation.
+        
+        :param surface_area: Surface area exposed to dust (m^2)
+        :param time_step: Time step for accumulation updates (s)
+        """
+        # Setup world
+        self.world = world
+        
+        # Lunar Dust behaviour options
+        self.limit_deposition = True
+        
+        
+        # Fixed parameters for dust behavior
+        self.dust_density = 1500  # (kg/m^3) - 
+        self.dust_diameter = 40e-6  # (m) - Approximate lunar dust particle diameter
+        self.altitude_to_deposition_rate = 500
+        
+        # Altitude-to-accumulation rate interpolation data points
+        alt_points = np.array([0, 0.6, 1, 1.9, 100])  # Altitudes (m)
+        taux_points = np.array([10000, 500, 100, 20, 0.001])*1e-5  # Corresponding accumulation rates
+        # at altitude 0 the accumulation rate has been set as 10000. there's no data on altitudes lower than 0.6 m, 
+        # this is only to start the interpolation at zero
+        
+        # Create an interpolation function for accumulation rate based on altitude
+        self.pchip_interp = PchipInterpolator(alt_points, taux_points)
+        
+       # Create an interpolation function for accumulation limit (by dust diameter) in a year based on data 
+        if self.limit_deposition:
+            print("DUST COVERAGE LIMITED BY DUST SIZE")
+            dust_radius_points = np.array([10, 50, 100]) 
+            limit_points = np.array([15, 3, 1.5])
+            self.pchip_limit = PchipInterpolator(dust_radius_points, limit_points)
+            self.max_coverage = self.pchip_limit(self.dust_diameter*1e6/2) / 100
+            print("Max surface coverage for ", self.dust_diameter, " m dust: ", self.max_coverage)
+
+        else:
+            self.max_coverage = 1
+
+        # Speed-to-accumulation rate interpolation data points 
+        speed_points = np.array([0.5, 1, 3, 5, 7])
+        altitude_reached_points = np.array([0.015, 0.024, 0.32, 1.32, 3.2])
+        
+        # Create an interpolation function for accumulation rate based on speed
+        self.pchip_altitude_reached = PchipInterpolator(speed_points, altitude_reached_points)
+        
+        # Variables for dust accumulation tracking
+        self.coverage = 0.0  # (%) Covered surface
+        self.taux = 0.0  # Current accumulation rate
+        self.total_time_passed = 0 
+        self.prev_simulation_time = 0
+                
+        # Data for color changing
+        self.retrieved_color = False
+        self.final_color = Gf.Vec3f((0.85, 0.83, 0.8))
+        self.final_roughness = 1.0
+        
+        
+        self.reduction_altitude = 0.0 ###### for testing purposes
+
+        
+    def plot(self) :
+    
+        # Print Plots (choose one)
+        plt.figure()
+        plt.plot(np.linspace(0,1.9, 100), self.pchip_interp(np.linspace(0,1.9, 100)) * 1e5)
+        plt.grid(True)
+        plt.xlabel("Altitude")
+        plt.ylabel("Deposition rate")
+        plt.title("Deposition rate by altitude")
+        plt.show()
+    
+        plt.figure()
+        plt.plot(np.linspace(0, 7, 100), self.pchip_altitude_reached(np.linspace(0, 7, 100)))
+        plt.grid(True)
+        plt.xlabel("Speed (m/s)")
+        plt.ylabel("Altitude reached")
+        plt.title("Deposition rate by altitude")
+        plt.show()
+    
+        if self.limit_deposition:
+            plt.figure()
+            plt.plot(np.linspace(10,100, 100), self.pchip_limit(np.linspace(10,100, 100)))
+            plt.grid(True)
+            plt.xlabel("Dust diameter")
+            plt.ylabel("Max possible covered surface (%)")
+            plt.title("Max covered surface by dust diameter")
+            plt.show()
+
+        
+
+    
+    def update(self, simulation_time: float, prim: Usd.Prim):
+        """
+        Updates the dust accumulation and surface coverage based on the current altitude.
+        
+        :param time_elapsed: Time elapsed since the last update (s)
+        :param prim: The USD Prim representing the object in the simulation
+        """
+        
+        time_elapsed = (simulation_time - self.prev_simulation_time)
+        self.total_time_passed += time_elapsed #/ (86400*365)
+        self.prev_simulation_time = simulation_time
+
+        if self.total_time_passed < 3*60*60:
+            print("Time Passed: ", round(self.total_time_passed / 60, 2), " minutes")
+        elif self.total_time_passed < 86400*3:
+            print("Time Passed: ", round(self.total_time_passed / (60*60), 2), " hours")
+        elif self.total_time_passed < 86400*365/2:
+            print("Time Passed: ", round(self.total_time_passed / 86400, 2), " days")
+        else: 
+            print("Time Passed: ", round(self.total_time_passed / (86400*365), 2), " years")
+
+
+        ################## WARNING
+        # The altitude is calculated with the distance from the ground where the ground coordinates come from a raycast from under it
+        # However, the environment of simulation is very large and to visualize better the prims, they have to be of an unrealistic size:
+        # this means that the lunar dust accumulation is not at all representative of its effects. We decided, for the sake of this test, to
+        # reduce manually the altitude calculations in order for it to provide a good value results in the tests. The testing prim appears 
+        # to be at altitude 28 m, so we will reduce it 
+        altitude = self.get_surface_altitude(prim) - self.reduction_altitude ################### WARNING
+        print("Altitude: ", round(altitude,2), " m")
+        
+        self.taux = self.compute_dust_accumulation_rate(altitude) # + compute_dust_accumulation_rate_by_movement(speed) ## WORK IN PROGRESS
+        print("Taux de deposition: ", round(self.taux*1e5, 2), " micrograms/cm^2*y")
+
+        self.update_coverage(time_elapsed)
+        print("Covered surface by dust: ", self.coverage*100, "%")
+        print("")
+
+            
+    def get_surface_altitude(self, prim: Usd.Prim) -> float:
+        """
+        Retrieves the altitude of a given prim by extracting its world position.
+
+        :param prim: The USD Prim to retrieve the position for.
+        :return: Altitude (z-coordinate in meters)
+        """
+        xform = UsdGeom.Xformable(prim)
+        time = Usd.TimeCode.Default() # The time at which we compute the bounding box
+        world_transform: Gf.Matrix4d = xform.ComputeLocalToWorldTransform(time)
+        translation: Gf.Vec3d = world_transform.ExtractTranslation()
+        #rotation: Gf.Rotation = world_transform.ExtractRotation()
+        #scale: Gf.Vec3d = Gf.Vec3d(*(v.GetLength() for v in world_transform.ExtractRotationMatrix()))  
+
+        # Perform raycast to get the real altitude
+        # raycast starts from way underground under the surface and goes in the Z+ direction in order to hit the ground under the surface
+        self.world.step(render=True)
+        
+
+        direction = (0,0,1)
+        origin = carb.Float3(translation[0], translation[1], -100) 
+        max_distance = 1000000000 
+        
+        
+        hit_info = get_physx_scene_query_interface().raycast_closest(origin, direction, max_distance+1)
+        self.world.step(render=True)
+        if hit_info["hit"]:
+            ground_coordinates = hit_info["position"]
+        else:
+            print("object has no ground under it")
+            ground_coordinates = (carb.Float3(translation[0], translation[1], -100000))
+
+        altitude = translation[2]-ground_coordinates[2] 
+        return altitude
+
+    def compute_dust_accumulation_rate(self, altitude: float) -> float:
+        """
+        Computes the dust accumulation rate based on the current altitude using interpolation.
+
+        :param altitude: Current altitude (m)
+        :return: Accumulation rate (kg/m^2*y)
+        """
+        return self.pchip_interp(altitude)
+    
+    def compute_dust_accumulation_rate_by_movement(self, speed: float) -> float: ### WORK IN PROGRESS
+        """
+        Computes the dust accumulation rate based on the current speed using interpolation.
+
+        :param altitude: Current speed (m/s)
+        :return: Accumulation rate (kg/m^2*y)
+        """
+        return self.pchip_altitude_reached(speed) * self.altitude_to_deposition_rate
+        
+    def update_coverage(self, time_elapsed: float):
+        """
+        Updates the surface coverage percentage based on dust accumulation.
+        
+        :param time_elapsed: Time elapsed since the last update (s)
+        """
+        # Ensure taux and other parameters are defined
+        if self.taux <= 0:
+            return
+
+        # Compute the fraction of the surface covered over time
+        coverage_increase = (3/2) * (self.taux / ( self.dust_diameter * self.dust_density)) * \
+                            (time_elapsed / (365 * 24 * 3600))  # Convert to years
+        
+        # Update total coverage
+        self.coverage = min(self.max_coverage, self.coverage + coverage_increase)  # Ensure it doesn't exceed 100%
+
+
+    def reset_accumulation(self):
+        """
+        Resets the dust accumulation and coverage to zero.
+        """
+        self.accumulated_dust = 0.0
+        self.coverage = 0.0
+
+    def update_color(self, shader_path):
+        if self.retrieved_color is False:
+            self.original_color = prims_utils.get_prim_attribute_value(shader_path, 'inputs:diffuseColor')
+            self.original_roughness = prims_utils.get_prim_attribute_value(shader_path, 'inputs:roughness')
+            self.retrieved_color = True
+            
+        new_color = self.original_color + self.coverage * (self.final_color - self.original_color)
+        #print("New color: ", new_color)
+        new_color = Gf.Vec3f(float(new_color[0]), float(new_color[1]), float(new_color[2]))
+
+        new_roughness = self.original_roughness + self.coverage * (self.final_roughness - self.original_roughness)
+        #print("New roughness: ", new_roughness)
+        
+        prims_utils.set_prim_attribute_value(shader_path, 'inputs:diffuseColor', new_color)
+        prims_utils.set_prim_attribute_value(shader_path, 'inputs:roughness', new_roughness)
+
+        self.world.render()
+
+    def set_color(self, shader_path, new_color):
+        new_color = Gf.Vec3f(float(new_color[0]), float(new_color[1]), float(new_color[2]))
+        prims_utils.set_prim_attribute_value(shader_path, 'inputs:diffuseColor', new_color)
